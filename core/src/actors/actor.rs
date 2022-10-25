9309421eb4f2793ef31c7ed4d000eb22c56d0c86
Joe McCain III <jo3mccain@icloud.com>
2022-10-25 16:27:22 +0000

    Update

--- core/src/actors/actor.rs
+++ core/src/actors/actor.rs
@@ -6,6 +6,13 @@
 */
 use crate::components::identities::Appellation;
 
+pub trait Transformation<S> {
+    fn data(&self) -> Vec<S>;
+    fn transform<T>(&self, catalyst: fn(&S) -> T) -> crate::BoxResult<Vec<T>> {
+        let res = self.data().iter().map( |i| catalyst(i)).collect::<Vec<_>>();
+        Ok(res)
+    }
+}
 pub trait ActorSpec {
     fn appellation(&self) -> Appellation;
     fn justification(&self) -> serde_json::Value;
--- core/src/core/utils.rs
+++ core/src/core/utils.rs
@@ -10,14 +10,15 @@ pub use crate::crypto::{hashes::utils::*, keypairs::utils::*};
 pub use crate::times::utils::*;
 
 pub(crate) mod configure {
-    use crate::{ConfigFile, ConfigFileVec};
+    use crate::{BoxResult, ConfigFile, ConfigFileVec};
+    
     use glob::glob;
 
     // Gather configuration files following the specified pattern and collect them into a vector
     pub fn collect_config_files(pattern: &str, required: bool) -> ConfigFileVec {
         glob(pattern)
-            .unwrap()
-            .map(|path| ConfigFile::from(path.unwrap()).required(required))
+            .expect("")
+            .map(|p| ConfigFile::from(p.expect("Failed to read the pathbuf")).required(required))
             .collect::<Vec<_>>()
     }
 }
