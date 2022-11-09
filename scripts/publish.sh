a76e792d99a721045f2166bc70207fa3fa503446
Joe McCain III <jo3mccain@icloud.com>
2022-11-09 16:30:41 +0000

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-core
-cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-crypto
-cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-derive
-cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-macros
-cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys
+cargo publish --all-features --color always --jobs 1 --verbose -p scsys-core
+cargo publish --all-features --color always --jobs 1 --verbose -p scsys-crypto
+cargo publish --all-features --color always --jobs 1 --verbose -p scsys-derive
+cargo publish --all-features --color always --jobs 1 --verbose -p scsys-macros
+cargo publish --all-features --color always --jobs 1 --verbose -p scsys
