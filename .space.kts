951294668fefa4ffe50841e38cec9c8d425a7812
Joe McCain III <jo3mccain@icloud.com>
2022-11-09 16:20:24 +0000

    Update

--- .space.kts
+++ .space.kts
@@ -31,11 +31,12 @@ job("Publish (crates)") {
         shellScript {
             interpreter = "/bin/bash"
             content = """
-                cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-core
-                cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-crypto
-                cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-derive
-                cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys-macros
-                cargo publish --all-features --color always --jobs 1 --token ${'$'}CARGO_REGISTRY_TOKEN --verbose -p scsys
+                cargo login ${'$'}CARGO_REGISTRY_TOKEN
+                cargo publish --all-features --color always --jobs 1 --verbose -p scsys-core
+                cargo publish --all-features --color always --jobs 1 --verbose -p scsys-crypto
+                cargo publish --all-features --color always --jobs 1 --verbose -p scsys-derive
+                cargo publish --all-features --color always --jobs 1 --verbose -p scsys-macros
+                cargo publish --all-features --color always --jobs 1 --verbose -p scsys
             """
         }
     }
