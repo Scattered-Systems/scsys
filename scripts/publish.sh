6aabd79c14a1107143c82969e262544f099b4f25
FL03 <jo3mccain@icloud.com>
2022-11-18 15:39:59 -0600

    Update: scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+cargo publish --all-features --jobs 1 --verbose -p scsys-agents
 cargo publish --all-features --jobs 1 --verbose -p scsys-core
 cargo publish --all-features --jobs 1 --verbose -p scsys-crypto
 cargo publish --all-features --jobs 1 --verbose -p scsys-derive
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+rustup update
 sudo apt update -y && sudo apt upgrade -y
