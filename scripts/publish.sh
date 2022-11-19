40ba085b571a9a506b7da872bdaab1e7a08b3f3c
Joe McCain III <jo3mccain@icloud.com>
2022-11-19 17:49:48 +0000

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,5 @@ cargo publish --all-features --jobs 1 --verbose -p scsys-agents
 cargo publish --all-features --jobs 1 --verbose -p scsys-core
 cargo publish --all-features --jobs 1 --verbose -p scsys-crypto
 cargo publish --all-features --jobs 1 --verbose -p scsys-derive
+cargo publish --all-features --jobs 1 --verbose -p scsys-gen
 cargo publish --all-features --jobs 1 --verbose -p scsys-macros
--- scsys/src/lib.rs
+++ scsys/src/lib.rs
@@ -22,6 +22,8 @@ pub mod prelude {
     pub use super::agents::{self, catalysts::*, connect::*, handlers::*, justify::*, messages::*};
     #[cfg(feature = "crypto")]
     pub use super::crypto::{self, hash::*, keys::*};
+    #[cfg(feature = "gen")]
+    pub use super::gen::*;
     #[cfg(feature = "core")]
     pub use super::{
         accounts::*, addresses::*, errors::*, events::*, extract::*, identities::*, loggers::*,
@@ -29,8 +31,6 @@ pub mod prelude {
     };
     #[cfg(feature = "derive")]
     pub use scsys_derive::*;
-    #[cfg(feature = "gen")]
-    pub use super::gen::*;
     #[cfg(feature = "macros")]
     pub use scsys_macros::*;
     // Extras
@@ -40,6 +40,4 @@ pub mod prelude {
     pub use chrono;
     #[cfg(feature = "config")]
     pub use config;
-
-    
 }
