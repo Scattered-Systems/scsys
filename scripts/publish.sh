37cf17ab922fb232e307f9e27122d6b514a99318
Joe McCain III <jo3mccain@icloud.com>
2022-11-21 15:54:23 +0000

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-cargo publish --all-features --jobs 1 --verbose -p scsys-agents
 cargo publish --all-features --jobs 1 --verbose -p scsys-core
 cargo publish --all-features --jobs 1 --verbose -p scsys-crypto
 cargo publish --all-features --jobs 1 --verbose -p scsys-derive
 cargo publish --all-features --jobs 1 --verbose -p scsys-gen
 cargo publish --all-features --jobs 1 --verbose -p scsys-macros
+cargo publish --all-features --jobs 1 --verbose -p scsys
