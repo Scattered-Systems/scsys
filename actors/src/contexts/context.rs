17641f0844420560e66ef7fd2d76d3dbff6ed473
Joe McCain III <jo3mccain@icloud.com>
2022-12-04 16:10:58 +0000

    Update

--- actors/src/contexts/context.rs
+++ actors/src/contexts/context.rs
@@ -1,8 +1,7 @@
 /*
     Appellation: context <module>
-    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
-    Description:
-        ... Summary ...
+    Contrib: FL03 <jo3mccain@icloud.com>
+    Description: ... Summary ...
 */
 use super::Configurable;
 use serde::{Deserialize, Serialize};
