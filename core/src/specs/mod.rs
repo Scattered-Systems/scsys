79016dff065f48ce2efa9b36f527bfd4ad0e37c1
FL03 <jo3mccain@icloud.com>
2022-12-08 13:21:49 -0600

    Update

--- core/src/specs/mod.rs
+++ core/src/specs/mod.rs
@@ -9,8 +9,7 @@ pub use self::{addressable::*, misc::*};
 pub(crate) mod addressable;
 
 pub(crate) mod misc {
-    use crate::{chrono_into_bson, Appellation, ChronoDateTime};
-    use chrono::Utc;
+    use crate::Appellation;
 
     pub trait InputName {
         fn name(&self) -> String;
@@ -22,17 +21,6 @@ pub(crate) mod misc {
     pub trait ActorSpec<T> {
         fn appellation(&self) -> Appellation<T>;
         fn justification(&self) -> serde_json::Value;
-    }
-
-    /// Interface for time-related data-structures
-    pub trait Temporal {
-        fn chrono_to_bson(&self, data: ChronoDateTime) -> bson::DateTime {
-            chrono_into_bson::<Utc>(data)
-        }
-        fn datetime() -> ChronoDateTime {
-            chrono::Utc::now()
-        }
-        fn timestamp(&self) -> i64; // Recall an objects time of creation
     }
 
     /// Quickly derive elligible naming schematics for the desired structure
