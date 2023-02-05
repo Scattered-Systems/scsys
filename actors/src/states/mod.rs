3f3e633fd608b6944864660ef6911ea2b28386d7
FL03 <jo3mccain@icloud.com>
2023-02-04 19:54:34 -0600

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- actors/src/states/mod.rs
+++ actors/src/states/mod.rs
@@ -3,43 +3,43 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... Summary ...
 */
-pub use self::{specs::*, state::*};
+pub use self::state::*;
 
 pub(crate) mod state;
 
-pub(crate) mod specs {
-    use crate::messages::Message;
-    use std::sync::Arc;
 
-    pub trait StatePack: Default + ToString {
-        fn by_ref(&self) -> &Self {
-            self
-        }
-        fn by_ref_mut(&mut self) -> &mut Self {
-            self
-        }
+use crate::messages::Message;
+use std::sync::Arc;
+
+pub trait StatePack: Default + ToString {
+    fn by_ref(&self) -> &Self {
+        self
     }
-
-    pub trait Stateful<S: StatePack>: Clone + Default {
-        type Data;
-        fn by_ref(&self) -> Self {
-            self.clone()
-        }
-        fn by_ref_mut(&mut self) -> Self {
-            self.clone()
-        }
-        fn by_arc(self: Arc<Self>) -> Arc<Self> {
-            self
-        }
-        fn message(self) -> Message<Self::Data>;
-        fn state(self) -> S;
-        fn timestamp(self) -> i64;
-    }
-
-    pub trait StatefulExt<S: StatePack>: Stateful<S> {
-        fn now() -> i64 {
-            chrono::Utc::now().timestamp()
-        }
-        fn update_state(&mut self, msg: Option<Message<Self::Data>>, state: S) -> &Self;
+    fn by_ref_mut(&mut self) -> &mut Self {
+        self
     }
 }
+
+pub trait Stateful<S: StatePack>: Clone + Default {
+    type Data;
+    fn by_ref(&self) -> Self {
+        self.clone()
+    }
+    fn by_ref_mut(&mut self) -> Self {
+        self.clone()
+    }
+    fn by_arc(self: Arc<Self>) -> Arc<Self> {
+        self
+    }
+    fn message(self) -> Message<Self::Data>;
+    fn state(self) -> S;
+    fn timestamp(self) -> i64;
+}
+
+pub trait StatefulExt<S: StatePack>: Stateful<S> {
+    fn now() -> i64 {
+        chrono::Utc::now().timestamp()
+    }
+    fn update_state(&mut self, msg: Option<Message<Self::Data>>, state: S) -> &Self;
+}
+
