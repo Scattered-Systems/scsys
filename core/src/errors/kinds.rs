68a469e63ea19f0a6c25c49cf25cd1b79ffad0dd
Joe McCain III <jo3mccain@icloud.com>
2024-03-03 11:52:32 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- core/src/errors/kinds.rs
+++ core/src/errors/kinds.rs
@@ -4,16 +4,16 @@
 */
 use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
-use strum::{Display, EnumIs, EnumIter, EnumVariantNames};
+use strum::{Display, EnumCount, EnumIs, EnumIter, VariantNames};
 
 #[derive(
     Clone,
     Debug,
     Deserialize,
     Display,
+    EnumCount,
     EnumIs,
     EnumIter,
-    EnumVariantNames,
     Eq,
     Hash,
     Ord,
@@ -21,6 +21,7 @@ use strum::{Display, EnumIs, EnumIter, EnumVariantNames};
     PartialOrd,
     Serialize,
     SmartDefault,
+    VariantNames
 )]
 #[serde(rename_all = "lowercase")]
 #[strum(serialize_all = "lowercase")]
