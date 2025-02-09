8247bb7919f203debc9bd8d913d7a84be462bedf
Joe McCain III <jo3mccain@icloud.com>
2025-02-09 09:25:57 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- core/src/hkt/applicative.rs
+++ core/src/hkt/applicative.rs
@@ -5,10 +5,17 @@
 //! # Applicative
 //!
 //!
-use super::functor::Functor;
-use super::HKT;
 
-use super::containers::*;
+pub trait HKT<U> {
+    type C; // Current Type
+    type T; // Type C swapped with U
+}
+
+pub trait Functor<U>: HKT<U> {
+    fn fmap<F>(&self, f: F) -> Self::T
+    where
+        F: Fn(&Self::C) -> U;
+}
 
 pub trait Applicative<U>: Functor<U> {
     fn pure_(value: U) -> Self::T
@@ -20,34 +27,157 @@ pub trait Applicative<U>: Functor<U> {
         Self: HKT<F>;
 }
 
-#[allow(unused_macros)]
-macro_rules! applicative {
-    ($($t:ident),* $(,)?) => {
+
+pub trait Monad<U>: Applicative<U> {
+    fn return_(x: U) -> Self::T
+    where
+        Self: HKT<U, C = U>,
+    {
+        Self::pure_(x)
+    }
+
+    fn bind<F>(&self, fs: F) -> Self::T
+    where
+        F: FnMut(&Self::C) -> Self::T;
+
+    fn join<T>(&self) -> T
+    where
+        Self: HKT<U, T = T, C = T>,
+        T: Clone,
+    {
+        self.bind(|x| x.clone())
+    }
+}
+
+
+
+
+/*
+    ************* Implementations *************
+*/
+#[macro_export]
+macro_rules! hkt {
+    ($($($p:ident)::*),* $(,)?) => {
         $(
-            applicative!(@impl $t);
+            $crate::hkt!(@impl $($p)::*);
         )*
     };
-    (@impl $t:ident) => {
-        impl<T, U> Applicative<U> for $t<T> {
-            fn pure_(value: U) -> Self::T {
-                $t::new(value)
-            }
+    (@impl $($p:ident)::*) => {
+        impl<T, U> HKT<U> for $($p)::*<T> {
+            type C = T;
+            type T = $($p)::*<U>;
+        }
+    };
+}
 
-            fn seq<F>(&self, fs: <Self as HKT<F>>::T) -> $t<U>
+#[allow(unused_macros)]
+macro_rules! functor {
+    ($($($p:ident)::*),* $(,)?) => {
+        $(
+            functor!(@impl $($p)::*);
+        )*
+    };
+    (@impl $($p:ident)::*) => {
+       impl<T, U> Functor<U> for $($p)::*<T> {
+            fn fmap<F>(&self, f: F) -> $($p)::*<U>
             where
-                F: Fn(&<Self as HKT<U>>::C) -> U,
+                F: Fn(&T) -> U,
             {
-                let v = fs(self);
-                $t::new(v)
+                $($p)::*::new(f(self))
             }
         }
     };
 }
 
-#[cfg(feature = "alloc")]
-applicative!(Arc, Box, Rc);
+#[allow(unused_macros)]
+macro_rules! applicative {
+    ($($($p:ident)::*),* $(,)?) => {
+        $(
+            applicative!(@impl $($p)::*);
+        )*
+    };
+    (@impl $($p:ident)::*) => {
+        impl<T, U> Applicative<U> for $($p)::*<T> {
+            fn pure_(value: U) -> Self::T {
+                $($p)::*::new(value)
+            }
 
-impl<T, U> Applicative<U> for Option<T> {
+            fn seq<F>(&self, fs: <Self as HKT<F>>::T) -> $($p)::*<U>
+            where
+                F: Fn(&<Self as HKT<U>>::C) -> U,
+            {
+                let v = fs(self);
+                $($p)::*::new(v)
+            }
+        }
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! monad {
+    ($($($p:ident)::*),* $(,)?) => {
+        $(
+            monad!(@impl $($p)::*);
+        )*
+    };
+    (@impl $($p:ident)::*) => {
+        impl<T, U> Monad<U> for $($p)::*<T> {
+            fn bind<F>(&self, mut fs: F) -> $($p)::*<U>
+            where
+                F: FnMut(&T) -> $($p)::*<U>,
+            {
+                fs(self)
+            }
+        }
+    };
+}
+
+hkt!(core::option::Option);
+
+#[cfg(feature = "alloc")]
+hkt!(
+    alloc::sync::Arc,
+    alloc::boxed::Box,
+    alloc::rc::Rc,
+    alloc::vec::Vec
+);
+
+#[cfg(feature = "alloc")]
+functor!(alloc::sync::Arc, alloc::boxed::Box, alloc::rc::Rc);
+
+#[cfg(feature = "alloc")]
+applicative!(alloc::sync::Arc, alloc::boxed::Box, alloc::rc::Rc);
+
+#[cfg(feature = "alloc")]
+monad!(alloc::sync::Arc, alloc::boxed::Box, alloc::rc::Rc);
+
+impl<T, U> Functor<U> for Option<T> {
+    fn fmap<F>(&self, f: F) -> Option<U>
+    where
+        F: Fn(&T) -> U,
+    {
+        if let &Some(ref value) = self {
+            return Some(f(value));
+        }
+        None
+    }
+}
+
+#[cfg(feature = "alloc")]
+impl<T, U> Functor<U> for Vec<T> {
+    fn fmap<F>(&self, f: F) -> Vec<U>
+    where
+        F: Fn(&T) -> U,
+    {
+        let mut result = Vec::with_capacity(self.len());
+        for value in self {
+            result.push(f(value));
+        }
+        result
+    }
+}
+
+impl<T, U> Applicative<U> for core::option::Option<T> {
     fn pure_(value: U) -> Self::T {
         Some(value)
     }
@@ -67,16 +197,16 @@ impl<T, U> Applicative<U> for Option<T> {
 }
 
 #[cfg(feature = "alloc")]
-impl<T, U> Applicative<U> for Vec<T> {
+impl<T, U> Applicative<U> for alloc::vec::Vec<T> {
     fn pure_(value: U) -> Self::T {
         vec![value]
     }
 
-    fn seq<F>(&self, fs: <Self as HKT<F>>::T) -> Vec<U>
+    fn seq<F>(&self, fs: <Self as HKT<F>>::T) -> alloc::vec::Vec<U>
     where
         F: Fn(&T) -> U,
     {
-        let mut result = Vec::new();
+        let mut result = alloc::vec::Vec::new();
         for (i, f) in fs.into_iter().enumerate() {
             let v = (f)(&self[i]);
             result.push(v)
@@ -84,3 +214,48 @@ impl<T, U> Applicative<U> for Vec<T> {
         return result;
     }
 }
+
+impl<T, U> Monad<U> for core::option::Option<T> {
+    fn bind<F>(&self, mut fs: F) -> core::option::Option<U>
+    where
+        F: FnMut(&T) -> core::option::Option<U>,
+    {
+        match self {
+            Some(x) => fs(x),
+            None => None,
+        }
+    }
+}
+
+#[cfg(feature = "alloc")]
+impl<T, U> Monad<U> for alloc::vec::Vec<T> {
+    fn bind<F>(&self, mut fs: F) -> Vec<U>
+    where
+        F: FnMut(&T) -> alloc::vec::Vec<U>,
+    {
+        let mut v = alloc::vec::Vec::new();
+        for x in self {
+            v.extend(fs(x));
+        }
+        v
+    }
+}
+
+#[allow(unused_imports)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(feature = "alloc")]
+    #[test]
+    fn test_hkt_vec() {
+        let v = Vec::from_iter(0..9);
+        let v2 = v.fmap(|x| (x + 1).to_string());
+        assert_eq!(v2, ["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
+        let v = Vec::return_(0);
+        let v2 = v.bind(|x| vec![x + 1]);
+        assert_eq!(v2, [1]);
+    }
+}
+
--- core/src/hkt/mod.rs
+++ core/src/hkt/mod.rs
@@ -3,73 +3,13 @@
     Contrib: FL03 <jo3mccain@icloud.com>
 */
 //! # Higher Kinded Types
-//!
-//!
 #[doc(inline)]
-pub use self::{applicative::*, functor::*, monad::*};
+pub use self::applicative::*;
 
 mod applicative;
-mod functor;
-mod monad;
+
+pub mod hkt;
 
 pub(crate) mod prelude {
-    pub use super::applicative::Applicative;
-    pub use super::functor::Functor;
-    pub use super::monad::Monad;
-    pub use super::HKT;
-}
-
-pub(crate) mod containers {
-    pub(crate) use core::option::Option;
-
-    #[cfg(feature = "alloc")]
-    pub(crate) use alloc::{boxed::Box, rc::Rc, sync::Arc, vec::Vec};
-}
-
-pub trait HKT<U> {
-    type C; // Current Type
-    type T; // Type C swapped with U
-}
-
-#[macro_export]
-macro_rules! hkt {
-    ($($($p:ident)::*),* $(,)?) => {
-        $(
-            $crate::hkt!(@impl $($p)::*);
-        )*
-    };
-    (@impl $($p:ident)::*) => {
-        impl<T, U> HKT<U> for $($p)::*<T> {
-            type C = T;
-            type T = $($p)::*<U>;
-        }
-    };
-}
-
-hkt!(containers::Option);
-
-#[cfg(feature = "alloc")]
-hkt!(
-    containers::Arc,
-    containers::Box,
-    containers::Rc,
-    containers::Vec
-);
-
-#[allow(unused_imports)]
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[cfg(feature = "alloc")]
-    #[test]
-    fn test_hkt_vec() {
-        let v = Vec::from_iter(0..9);
-        let v2 = v.fmap(|x| (x + 1).to_string());
-        assert_eq!(v2, ["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-
-        let v = Vec::return_(0);
-        let v2 = v.bind(|x| vec![x + 1]);
-        assert_eq!(v2, [1]);
-    }
+    pub use super::applicative::*;
 }
