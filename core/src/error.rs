dbb654f0b76aafbfba2ffb65312d3239bd0aeb38
FL03 <jo3mccain@icloud.com>
2025-05-21 14:57:07 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- core/src/error.rs
+++ core/src/error.rs
@@ -2,6 +2,7 @@
     Appellation: error <module>
     Contrib: @FL03
 */
+#[allow(unused_imports)]
 #[doc(inline)]
 pub use self::{core_error::*, raw_error::*, std_error::*};
 
@@ -10,8 +11,7 @@ mod core_error;
 /// the this module implements a raw, generic error type wrapper
 mod raw_error;
 /// this module implements an alternative error type that uses some kind to distinguish
-/// between different error types.
-#[cfg(feature = "alloc")]
+/// between different error types
 mod std_error;
 
 pub trait ErrorKind: core::fmt::Debug {}
