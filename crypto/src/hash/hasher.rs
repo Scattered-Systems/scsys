7716d106ae2b83fb4737d477ab55ac9fc4626289
FL03 <jo3mccain@icloud.com>
2025-05-22 02:38:58 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- crypto/src/hash/hasher.rs
+++ crypto/src/hash/hasher.rs
@@ -3,15 +3,37 @@
     Contrib: @FL03
 */
 
-/// a generic interface for all compatible hashers
+/// A [`Hasher`] defines a common interface for defining specific hashing algorithm.
 pub trait Hasher {
-    type Hash;
-    /// initialize the hasher
-    fn hash(data: impl AsRef<[u8]>) -> Self::Hash;
-    /// finalize the hash and return the result
-    fn finalize(&self) -> crate::CryptoResult<Self::Hash>;
+    /// the output type of the hasher
+    type Output;
     /// reset the hasher to its initial state
-    fn reset(&mut self) -> &mut Self;
+    fn clean(&mut self) -> &mut Self;
+    /// finalize the hash and return the result
+    fn finish(&self) -> crate::CryptoResult<Self::Output>;
     /// update the hasher with new data
-    fn update(&mut self, data: impl AsRef<[u8]>) -> &mut Self;
+    fn include(&mut self, data: impl AsRef<[u8]>) -> &mut Self;
+}
+
+mod impl_blake3 {
+    use crate::hash::H256;
+
+    impl super::Hasher for blake3::Hasher {
+        type Output = H256;
+
+        fn clean(&mut self) -> &mut Self {
+            self.reset()
+        }
+
+        fn finish(&self) -> crate::CryptoResult<Self::Output> {
+            // finalize the `blake3` hasher
+            let hash = self.finalize();
+            // convert the hash into the defined type before returning it
+            Ok(hash.into())
+        }
+
+        fn include(&mut self, data: impl AsRef<[u8]>) -> &mut Self {
+            self.update(data.as_ref())
+        }
+    }
 }
